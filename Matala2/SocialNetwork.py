206ec8c8bd69235130a885efe5a681c2e17dfaf0
Nadav shabtai <132436784+nadav034@users.noreply.github.com>
2024-02-28 14:06:59 +0200

    Update SocialNetwork.py

--- Matala2/SocialNetwork.py
+++ Matala2/SocialNetwork.py
@@ -2,17 +2,24 @@ from User import User
 
 
 class SocialNetworkMeta(type):
-    def __call__(self, *args, **kwargs):
-        pass
+    _instances = {}
+    """Create or return the singleton instance of the class- using the singleton design pattern """
+    def __call__(cls, *args, **kwargs):
+        # If an instance of the class doesn't exist, create one and store it in _instances
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
 
 
-class SocialNetwork:
+class SocialNetwork(metaclass=SocialNetworkMeta):
+    """Initialize a new SocialNetwork instance."""
     def __init__(self, name_of_network: str):
         self.name = name_of_network
         self.user_list: list[User] = []
         print(f"The social network {name_of_network} was created!")
 
     def __str__(self):
+        """Return a string representation of the social network."""
         resault_str =(f"{self.name} social network:\n")
         for user in self.user_list:
             resault_str += user.__str__()+ "\n"
@@ -20,6 +27,7 @@ class SocialNetwork:
 
 
     def sign_up(self, username: str, password: str):
+        """Sign up a new user. Returns the created User object or False."""
         if len(password) < 4 or len(password) > 8:
             return False
         if self.user_list.count(username):
@@ -29,7 +37,9 @@ class SocialNetwork:
         self.log_in(u1,password)
         return u1
 
+
     def log_out(self, user_name):
+        """Log out a user. Returns True if successful, False otherwise."""
         filtered = list(filter(lambda user: user.name == user_name, self.user_list))
         if len(filtered) != 1:
             return False
@@ -38,6 +48,7 @@ class SocialNetwork:
         print(f"{user.name} disconnected")
 
     def log_in(self, user_name, password):
+        """Log in a user. Returns True if successful, False otherwise."""
         filtered = list(filter(lambda user: user.name == user_name and user.password == password, self.user_list))
         if len(filtered) != 1:
             return False
